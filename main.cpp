79cde99827ab66c0efce80e72f4eff3ae2f19ae5
Dforgeek <1812stepanovsky@gmail.com>
2022-05-24 14:19:43 +0300

    сделал работу с векторами

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include "mallocator.h"
 #include <vector>
+#include <queue>
+#include <list>
+#include <map>
 
-int main(){
-    std::vector<long long, Mollocator<long long,200>> v;
+int main() {
+    std::vector<long long, Mollocator<long long, 20000>> v(5);
 
-    for (int i=0; i<100; ++i){
+    for (int i = 0; i < 2000; ++i) {
         v.push_back(i);
     }
 
-    for (long long i : v){
-        std::cout << i<<'\n';
+    for (int i = 0; i < v.size(); ++i) {
+        std::cout << v[i] << '\n';
     }
+
+/*    std::map<int, int, std::less<>, Mollocator<int, 100>> l;
+
+    for (int i=0; i<50; ++i){
+        l.insert({i, i+5});
+    }
+    for (int i=0; i<50; ++i){
+        std::cout<<l[i]<<'\n';
+    }*/
+
     return 0;
 }
--- mallocator.h
+++ mallocator.h
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <cassert>
-#include "mylist.h"
+
 
 template<class T, size_t TotalElems>
 class Mollocator {
 private:
-    using Node = typename Stack::Node;
-    Stack m_freeStack;
+    struct Header {
+        Header *next;
+    };
+    union Chunk {
+        Header *header;
+        T data;
+    };
 
-    void *m_start_ptr = nullptr;
+    std::size_t m_chunkAmount{TotalElems};
     std::size_t m_chunkSize = sizeof(T);
-protected:
-    std::size_t m_totalSize = TotalElems * m_chunkSize;
-    std::size_t m_used = 0;
-    std::size_t m_peak = 0;
+
+    Chunk *m_memory{nullptr};
+    Chunk *m_start{nullptr};
+    Chunk *m_end{nullptr};
+
+    Header *m_stackTop{nullptr};
+
 public:
     //TYPE DEFINITIONS
     typedef std::size_t size_type;
     typedef T *pointer;
     typedef T value_type;
+
 
     template<class U>
     struct rebind {
@@ -26,58 +35,77 @@ public:
     };
 
     Mollocator() {
-        assert(m_chunkSize >= 4 && "Chunk size must be greater or equal to 4");
-        assert(m_totalSize % m_chunkSize == 0 && "Total Size must be a multiple of Chunk Size");
-        m_start_ptr = malloc(m_totalSize);
-        Reset();
+        m_memory = (Chunk *) malloc(m_chunkSize * m_chunkAmount);
+        m_start = m_memory + 0;
+
+        m_end = m_start + m_chunkAmount;
+        m_end->header = nullptr;
+        m_stackTop = reinterpret_cast<Header *>(m_start);
+        m_stackTop->next = nullptr;
     }
+
 
     Mollocator(const Mollocator &copy) {
-        m_freeStack = copy.m_freeStack;
-        m_start_ptr = copy.m_start_ptr;
+        m_chunkAmount = copy.m_chunkAmount;
         m_chunkSize = copy.m_chunkSize;
-        m_totalSize = copy.m_totalSize;
-        m_used = copy.m_used;
-        m_peak = copy.m_peak;
+        m_memory = (Chunk *) malloc(m_chunkSize * m_chunkAmount);
+        m_start = copy.m_start;
+        m_end = copy.m_end;
+        m_stackTop = copy.m_stackTop;
+        m_stackTop->next = nullptr;
     }
 
+    /*   ~Mollocator() {
+           free(m_memory);
+       }*/
 
-    ~Mollocator() {
-        free(m_start_ptr);
-    }
 
-    pointer allocate(const std::size_t n) {
-        assert(n!=0 && "n cannot be zero!");
-        Node * freePosition;
-        for (int i = 0; i < n; i++) {
-            assert(m_freeStack.head != nullptr && "The pool allocator is full");
-            freePosition=m_freeStack.pop();
-            m_used += m_chunkSize;
-            m_peak = std::max(m_peak, m_used);
+    [[nodiscard]] pointer allocate(const std::size_t n) {
+        assert(n != 0 && "n cannot be zero!");
+        pointer freePosition = nullptr;
+        for (int i = 0; i < n; ++i) {
+            assert(m_stackTop != nullptr && "The pool allocator is full");
+            assert(n <= m_chunkAmount && "Can't allocate more than planned");
+
+            if (i == 0) {
+                freePosition = reinterpret_cast<pointer>(m_stackTop);
+            }
+
+            if (m_stackTop->next == nullptr) {
+                Chunk *nextChunk = reinterpret_cast<Chunk *>(m_stackTop) + 1;
+                Header *nextHead = reinterpret_cast<Header *>(nextChunk);
+
+
+                if (m_end > nextChunk) {
+                    m_stackTop->next = nextHead;
+                    nextHead->next = nullptr;
+                }
+            }
         }
-        return reinterpret_cast<pointer> (freePosition);
+        return freePosition;
     }
+
 
     void deallocate(pointer ptr, size_type n) {
-        for (int i = 0; i < n; i++) {
-            m_used -= m_chunkSize;
-            m_freeStack.push((Node *) ptr);
-            ptr++;
-        }
+        Chunk *blockToFree = (Chunk *) ptr;
 
+        assert(m_start <= blockToFree && "Pointer does not refer to this allocator");
+        assert((blockToFree + n) <= m_end && "Pointer does not refer to this allocator");
+
+        for (std::size_t i = 0; i < n; ++i) {
+
+            Chunk *freeBlock = blockToFree++;
+            Header *newHead = reinterpret_cast<Header *>(freeBlock);
+            newHead->next = m_stackTop;
+            m_stackTop = newHead;
+
+        }
     }
 
-    void Reset() {
-        m_used = 0;
-        m_peak = 0;
-        // Create a linked-list with all free positions
-        const int nChunks = m_totalSize / m_chunkSize;
-        for (int i = 0; i < nChunks; ++i) {
-            std::size_t address = (std::size_t) m_start_ptr + i * m_chunkSize;
-            m_freeStack.push((Node *) address);
-        }
 
-    }
 };
+
+
+
 
 
